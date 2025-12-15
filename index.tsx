a804d3e91b5658a6239ba715a4d12dd8308eca59
jadhav63 <djadhav.usa@gmail.com>
2025-12-15 01:49:39 -0700

    Create index.tsx

---
+++ index.tsx
@@ -0,0 +1,4 @@
+// This application has been converted to a static website.
+// Please open index.html directly.
+// React mounting logic has been removed to prevent "Could not find root element" errors.
+console.log('Static site loaded');
